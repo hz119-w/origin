43133b68274670efd486105ae911dc4ad1a103a3
hz119-w <1357595216@qq.com>
2024-11-11 02:05:32 +0800

    Create hzt.sh
    sh

---
+++ hzt.sh
@@ -0,0 +1,4 @@
+$ ssh -T git@github.com
+The authenticity of host 'github.com (52.74.223.119)' can't be established.
+RSA key fingerprint is SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8.
+Are you sure you want to continue connecting (yes/no/[fingerprint])? yes  
