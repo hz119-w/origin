e1bf6128af1e0ae85f2ff29fffef6ee015bf19dc
hz119-w <1357595216@qq.com>
2024-11-11 18:06:34 +0800

    Update shell_test.sh
    添加shell脚本的传参示例

--- shell_test.sh
+++ shell_test.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 echo "Hello World !"
+
+echo "Shell 传递参数实例！";
+echo "执行的文件名：$0";
+echo "第一个参数为：$1";
+echo "第二个参数为：$2";
+echo "第三个参数为：$3";
+
+# $ chmod +x test.sh 
+# $ ./test.sh 1 2 3
+# Shell 传递参数实例！
+# 执行的文件名：./test.sh
+# 第一个参数为：1
+# 第二个参数为：2
+# 第三个参数为：3
